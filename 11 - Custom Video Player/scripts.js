8bfc2fe3f1ccab836e49c8c09280fd70e641fb6b
James Lau <hkjl87@hotmail.co.uk>
2023-09-22 14:44:04 +0900

    toggleButton

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,6 +16,14 @@ if (video.paused){
     video.pause();
   }
 }
+
+function updateButton(){
+const icon = this.paused ? '►' : '❚ ❚';
+toggle.textContent = icon;
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
