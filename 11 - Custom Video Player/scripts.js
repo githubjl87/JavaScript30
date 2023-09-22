75e4e9aa1dd342f487787730cb544d5ac288f66a
James Lau <hkjl87@hotmail.co.uk>
2023-09-22 14:35:55 +0900

    video

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,3 +1,4 @@
+/* Get Our Elements */
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
@@ -5,3 +6,16 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+function togglePlay() {
+if (video.paused){
+  video.play();
+}
+  else{
+    video.pause();
+  }
+}
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
