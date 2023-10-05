73157fd1423ac3e636285e4d798359f6b3535165
James Lau <hkjl87@hotmail.co.uk>
2023-10-05 11:39:23 +0900

    displayEndTime

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -22,4 +24,12 @@ function displayTimeLeft(seconds) {
   const remainderSecs = seconds % 60;
   const display = `${mins}:${remainderSecs < 10 ? '0' : ''}${remainderSecs}`;
   timerDisplay.textContent = display;
+  document.title = display;
+}
+
+function displayEndTime(timeStamp) {
+  const end = new Date(timeStamp);
+  const endMins = end.getMinutes();
+  const endHr = end.getHours();
+  endTime.textContent = `Get back at ${endHr}:${endMins}`
 }
