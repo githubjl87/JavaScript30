b54e98922eda5d549db178e134ed06e16890526c
James Lau <hkjl87@hotmail.co.uk>
2023-10-05 12:03:28 +0900

    clearInterval

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,17 +1,19 @@
-let countDown;
+let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     if (secondsLeft < 0){
-      clearInterval(countDown);
+      clearInterval(countdown);
       return;
     }
     displayTimeLeft(secondsLeft);
@@ -31,5 +33,13 @@ function displayEndTime(timeStamp) {
   const end = new Date(timeStamp);
   const endMins = end.getMinutes();
   const endHr = end.getHours();
-  endTime.textContent = `Get back at ${endHr}:${endMins}`
+  const adjustedHr = endHr > 12 ? endHr - 12 : endHr;
+  endTime.textContent = `Get back at ${adjustedHr}:${endMins < 10 ? '0' : ''}${endMins}`
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
