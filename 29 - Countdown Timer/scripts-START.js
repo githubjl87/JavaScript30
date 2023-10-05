61b2024fb6315a1158e0725b5f68f733f1227b3e
James Lau <hkjl87@hotmail.co.uk>
2023-10-05 10:31:31 +0900

    displayTimeLeft

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,21 @@
+let countDown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log (now, then);
+  displayTimeleft(seconds);
 
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    if (secondsLeft < 0){
+      clearInterval(countDown);
+      return;
+    }
+    displayTimeleft(secondsLeft);
+  }, 1000);
+
+}
+
+function displayTimeleft(seconds) {
+  console.log (seconds);
 }
